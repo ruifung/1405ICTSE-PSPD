ebe22161b12ea30e78f17f0f8078d9992affbe15
0nepeop1e <0nepeop1e@moe9th.com>
2015-05-28 12:24:29 +0800

    guest scene

--- SSRS/main.c
+++ SSRS/main.c
@@ -2,4 +2,9 @@
 #include <Windows.h>
 
 #include "simcon.h"
+#include "menu_guest.h"
 
+int main(int argc, char * args[]){
+	SetConsoleTitle("Sport Space Rental System");
+	menu_start(menu_guest());
+}
