fdbe11eed7cc529b9f9c5215eb3ed0cd575c7143
Yip Rui Fung <ruifungyip@gmail.com>
2015-06-25 07:28:12 +0800

    Begin work on rsvp refs.

--- SSRS/courts.c
+++ SSRS/courts.c
@@ -10,8 +10,19 @@
 #include "courts.h"
 
 #define reservations_read(buffer,elemSize,count,file) fread_s(buffer, count * elemSize, elemSize, count, file);
-const int endianCheck = 1;
-#define is_bigendian() ( (*(char*)&endianCheck) == 0 )
+
+//Structs and types for internal use.
+typedef struct RSVP_REF RSVP_REF;
+typedef struct RSVP_LINK RSVP_LINK;
+struct RSVP_REF {
+	uint ref_num;
+	RSVP_LINK *list;
+};
+struct RSVP_LINK {
+	RESERVATION *item;
+	RSVP_LINK *prev;
+	RSVP_LINK *next;
+};
 
 COURT courts[COURTS_COUNT];
 char *reservations_file;
@@ -20,6 +31,11 @@ struct {
 	uint length;
 	RESERVATION **data;
 } reservations;
+struct {
+	uint lastRef;
+	uint refLen;
+	RSVP_REF *rsvpRef;
+} references;
 
 void utoa(unsigned int uint, char *byteArray);
 _Bool courts_load();
@@ -28,6 +44,8 @@ uint courts_getFirstBlockSlot(uint block, char courtId);
 int courts_cmpr(BST_CMPR_ARGS);
 bool courts_isBlockRangeEmptyRecur(BST_NODE *node, uint lowerBlock, uint upperBlock);
 uint courts_getReservationIndex(uint id);
+void courts_refLinkRecur(RSVP_LINK **link, RSVP_LINK *parent, RESERVATION *item);
+int courts_cmprRef(const RSVP_REF *item1, const RSVP_REF *item2);
 
 void courts_init(char *reservationsFile){
 	courts[0] = (COURT){ 0, COURT_TYPE_BATMINTON, 'A', 14, 44, 8.0f, NULL };
@@ -42,6 +60,8 @@ void courts_init(char *reservationsFile){
 	courts[9] = (COURT){ 9,  COURT_TYPE_FUTSAL, 'A', 14, 44, 20.0f, NULL };
 	courts[10] = (COURT){ 10, COURT_TYPE_FUTSAL, 'B', 14, 44, 20.0f, NULL };
 	courts[11] = (COURT){ 11, COURT_TYPE_FUTSAL, 'C', 14, 44, 20.0f, NULL };
+	references.lastRef = 0;
+	references.refLen = 0;
 	reservations.lastID = 0;
 	reservations.length = 0;
 	reservations.data = NULL;
@@ -50,6 +70,8 @@ void courts_init(char *reservationsFile){
 }
 
 _Bool courts_load() {
+	uint refCurLen = 0;
+	bool refSet = false;
 	RESERVATION *tmpPtr = NULL;
 	DWORD attr = GetFileAttributes(reservations_file);
 	if ((attr == INVALID_FILE_ATTRIBUTES)) {
@@ -63,20 +85,42 @@ _Bool courts_load() {
 	if (file == NULL)
 		return false;
 	reservations_read(&reservations.lastID, sizeof(uint), 1, file);
+	reservations_read(&references, sizeof(references), 1, file);
 	reservations_read(&reservations.length, sizeof(uint), 1, file);
+	references.rsvpRef = malloc(references.refLen * sizeof(references.refLen));
+	reservations.data = malloc(reservations.length * sizeof(tmpPtr));
 	if (reservations.length == 0)
 		return true;
-	reservations.data = malloc(reservations.length * sizeof(tmpPtr));
 	if (reservations.data == NULL)
 		return false;
 	for (unsigned long int i = 0; i < reservations.length; i++) {
+		refSet = false;
 		tmpPtr = malloc(sizeof(RESERVATION));
 		if (tmpPtr == NULL)
 			return false;
 		reservations_read(tmpPtr, sizeof(RESERVATION), 1, file);
 		reservations.data[i] = tmpPtr;
 		bst_addNode(&courts[tmpPtr->court_id].reservations,&(tmpPtr->startTime),sizeof(uint),tmpPtr,&courts_cmpr);
+		for (uint i = 0; i < refCurLen; i++) {
+			if (references.rsvpRef[i].ref_num == tmpPtr->ref_num) {
+				courts_refLinkRecur(&references.rsvpRef[i].list, NULL, tmpPtr);
+				refSet = true;
+				break;
+			}
+		}
+		if (!refSet) {
+			RSVP_REF newRef;
+			newRef.list = NULL;
+			newRef.ref_num = tmpPtr->ref_num;
+			courts_refLinkRecur(&newRef.list, NULL, tmpPtr);
+			RSVP_REF *tmpRef = realloc(references.rsvpRef, ++refCurLen * sizeof(RSVP_REF));
+			if (tmpRef == NULL)
+				return false;
+			references.rsvpRef = tmpRef;
+			references.rsvpRef[refCurLen - 1] = newRef;
+		}
 	}
+	qsort(references.rsvpRef, references.refLen, sizeof(RSVP_REF), &courts_cmprRef);
 	free(tmpPtr);
 	fclose(file);
 	return true;
@@ -90,11 +134,12 @@ _Bool courts_save() {
 	if (attr & FILE_ATTRIBUTE_DIRECTORY) {
 		_pause("ERROR:%s is a folder!", reservations_file);
 		return false;
-	}
+	} 
 	FILE *file = fopen(reservations_file, "wb");
 	if (file == NULL)
 		return false;
 	fwrite(&reservations.lastID, sizeof(uint), 1, file);
+	fwrite(&references, sizeof(references), 1, file);
 	fwrite(&reservations.length, sizeof(uint), 1, file);
 	for (uint i = 0; i < reservations.length; i++) {
 		fwrite(reservations.data[i], sizeof(RESERVATION), 1, file);
@@ -298,4 +343,38 @@ uint courts_getFirstBlockSlot(uint block, char courtId) {
 	getdate(date, false);
 	time_t start = mktime(date);
 	return (start / BLOCK_DURATION) + courts[courtId].startBlock;
+}
+
+//Due to this function, there will never be a ref num of 0;
+uint courts_newRefNum() {
+	uint ref = ++references.lastRef;
+	RSVP_REF *tmpArr = realloc(references.rsvpRef, ++references.refLen * sizeof(RSVP_REF));
+	if (tmpArr == NULL) {
+		references.lastRef--;
+		references.refLen--;
+		return 0;
+	}
+	return ref;
+}
+uint courts_countRefReservations(uint ref) {
+
+}
+void courts_getRefReservations(uint ref, RESERVATION **dataArray, uint arraySize) {
+
+}
+
+void courts_refLinkRecur(RSVP_LINK **link, RSVP_LINK *parent, RESERVATION *item) {
+	if (*link == NULL) {
+		*link = malloc(sizeof(RSVP_LINK));
+		(*link)->item = item;
+		(*link)->next = NULL;
+		(*link)->prev = parent;
+	}
+	else {
+		courts_refLinkRecur(&(*link)->next,*link,item);
+	}
+}
+
+int courts_cmprRef(const RSVP_REF *item1, const RSVP_REF *item2) {
+	return item1->ref_num - item2->ref_num;
 }
