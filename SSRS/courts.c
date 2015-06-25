6973b8e2656cea04016559a759dad778e9cc89c6
Yip Rui Fung <ruifungyip@gmail.com>
2015-06-25 13:49:02 +0800

    Fix some checks to prevent weird behavior when file does not exist.

--- SSRS/courts.c
+++ SSRS/courts.c
@@ -51,9 +51,10 @@ void courts_init(char *reservationsFile){
 	courts[11] = (COURT){ 11, COURT_TYPE_FUTSAL, 'C', 14, 44, 20.0f, NULL };
 	references.lastRef = 0;
 	references.refLen = 0;
+	references.rsvpRef = malloc(sizeof(RSVP_REF));
 	reservations.lastID = 0;
 	reservations.length = 0;
-	reservations.data = NULL;
+	reservations.data = malloc(sizeof(RESERVATION*));
 	reservations_file = reservationsFile;
 }
 
@@ -61,9 +62,12 @@ _Bool courts_load() {
 	uint refCurLen = 0;
 	bool refSet = false;
 	RESERVATION *tmpPtr = NULL;
+	//VERIFY FILE IS VALID well.. file.
 	DWORD attr = GetFileAttributes(reservations_file);
 	if ((attr == INVALID_FILE_ATTRIBUTES)) {
-		return false;
+		DWORD err = GetLastError();
+		if (err == ERROR_FILE_NOT_FOUND)
+			return true;
 	}
 	if (attr & FILE_ATTRIBUTE_DIRECTORY) {
 		_pause("ERROR:%s is a folder!", reservations_file);
@@ -72,10 +76,14 @@ _Bool courts_load() {
 	FILE *file = fopen(reservations_file, "rb");
 	if (file == NULL)
 		return false;
+
 	//Load reference number data
 	reservations_read(&references.lastRef, sizeof(uint), 1, file);
 	reservations_read(&references.refLen, sizeof(uint), 1, file);
-	references.rsvpRef = malloc(references.refLen * sizeof(RSVP_REF));
+	if (references.refLen > 0)
+		references.rsvpRef = realloc(references.rsvpRef, references.refLen * sizeof(RSVP_REF));
+	if (references.rsvpRef == NULL)
+		return false;
 	for (uint i = 0; i < references.refLen; i++) {
 		RSVP_REF tmpRef = { 0 };
 		uint nameLen = 0;
@@ -87,13 +95,12 @@ _Bool courts_load() {
 		tmpRef.list = NULL;
 		references.rsvpRef[i] = tmpRef;
 	}
+
 	//Load reservations
 	reservations_read(&reservations.lastID, sizeof(uint), 1, file);
 	reservations_read(&reservations.length, sizeof(uint), 1, file);
-	references.rsvpRef = malloc(references.refLen * sizeof(references.refLen));
-	reservations.data = malloc(reservations.length * sizeof(tmpPtr));
-	if (reservations.length == 0)
-		return true;
+	if (reservations.length > 0)
+		reservations.data = realloc(reservations.data, reservations.length * sizeof(tmpPtr));
 	if (reservations.data == NULL)
 		return false;
 	for (uint i = 0; i < reservations.length; i++) {
@@ -108,16 +115,21 @@ _Bool courts_load() {
 		if (ref == NULL) return false;
 		courts_refLinkRecur(&ref->list, NULL, tmpPtr);
 	}
-	qsort(references.rsvpRef, references.refLen, sizeof(RSVP_REF), &courts_cmprRef);
-	free(tmpPtr);
+
+	if (reservations.length > 0)
+		qsort(references.rsvpRef, references.refLen, sizeof(RSVP_REF), &courts_cmprRef);
+	if (tmpPtr != NULL)
+		free(tmpPtr);
 	fclose(file);
 	return true;
 }
 
 _Bool courts_save() {
 	DWORD attr = GetFileAttributes(reservations_file);
-	if ((attr == INVALID_FILE_ATTRIBUTES)) {
-		return false;
+	if (attr == INVALID_FILE_ATTRIBUTES) {
+		//DO NOTHING IF FILE NOT FOUND.. We are going to write the file anyway.
+		if (GetLastError() == ERROR_FILE_NOT_FOUND);
+		else return false;
 	}
 	if (attr & FILE_ATTRIBUTE_DIRECTORY) {
 		_pause("ERROR:%s is a folder!", reservations_file);
@@ -336,11 +348,15 @@ bool courts_delReservation(RESERVATION *reservation) {
 	//Shift elements after index to left by 1.
 	memmove(&reservations.data[index],&reservations.data[index+1],len);
 	//Shrink array memory size.
-	RESERVATION **newArr = realloc(reservations.data, --reservations.length * sizeof(newArr));
-	if (newArr == NULL)
-		return false;
-	else
-		reservations.data = newArr;
+	RESERVATION **newArr = NULL;
+	uint newSize = --reservations.length;
+	if (newSize > 0) {
+		newArr = realloc(reservations.data, newSize * sizeof(newArr));
+		if (newArr == NULL)
+			return false;
+		else
+			reservations.data = newArr;
+	}
 	//Free unused reservation data.
 	free(tmpData);
 	return true;
